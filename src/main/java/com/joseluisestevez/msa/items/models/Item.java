083208ff94c85a3d508a6fff27d8bb521da661d5
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-22 17:34:28 +0100

    Add new model item class

---
+++ src/main/java/com/joseluisestevez/msa/items/models/Item.java
@@ -0,0 +1,37 @@
+package com.joseluisestevez.msa.items.models;
+
+public class Item {
+	
+	private Product product;
+	private Integer quantity;
+
+	public Item() {
+		super();
+	}
+	
+	public Item(Product product, Integer quantity) {
+		super();
+		this.product = product;
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getTotal() {
+		return product.getPrice() * quantity.doubleValue();
+	}
+}
