9399a624524e4c13041a563bccfaf3e66725862f
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-22 17:27:50 +0100

    New Spring Boot project - Items

---
+++ src/test/java/com/joseluisestevez/msa/items/SpringbootServiceItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.msa.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
