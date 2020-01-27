7ef2efe5a4ba9e6f5c9db0e8ded8e638d68177f8
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-27 14:27:23 +0100

    Create Item Service with RestTemplate client

---
+++ src/main/java/com/joseluisestevez/msa/items/controllers/ItemController.java
@@ -0,0 +1,28 @@
+package com.joseluisestevez.msa.items.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joseluisestevez.msa.items.models.Item;
+import com.joseluisestevez.msa.items.service.ItemService;
+
+@RestController
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@GetMapping("/list")
+	public List<Item> list() {
+		return itemService.findAll();
+	}
+	
+	@GetMapping("/detail/{id}/quantity/{quantity}")
+	public Item detail(@PathVariable Long id, @PathVariable Integer quantity) {
+		return itemService.findById(id, quantity);
+	}
+}
