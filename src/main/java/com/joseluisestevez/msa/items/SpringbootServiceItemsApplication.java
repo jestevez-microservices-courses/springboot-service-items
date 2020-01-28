b8b5df8676214915fce8fff732e19f1493b51bab
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 11:33:46 +0100

    Load balancing with Ribbon

--- src/main/java/com/joseluisestevez/msa/items/SpringbootServiceItemsApplication.java
+++ src/main/java/com/joseluisestevez/msa/items/SpringbootServiceItemsApplication.java
@@ -2,12 +2,13 @@ package com.joseluisestevez.msa.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@RibbonClient(name = "service-products")
 public class SpringbootServiceItemsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServiceItemsApplication.class, args);
 	}
