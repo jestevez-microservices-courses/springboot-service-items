f264ab7e25d028026c0ab6b4253b4b43bbf17e3b
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 11:49:16 +0100

    Load balancing with Ribbon, remove url from ProductClientRest

--- src/main/java/com/joseluisestevez/msa/items/clients/ProductClientRest.java
+++ src/main/java/com/joseluisestevez/msa/items/clients/ProductClientRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.joseluisestevez.msa.items.models.Product;
 
-@FeignClient(name="service-products", url="localhost:8001")
+@FeignClient(name = "service-products")
 public interface ProductClientRest {
 
 	@GetMapping("/list")
