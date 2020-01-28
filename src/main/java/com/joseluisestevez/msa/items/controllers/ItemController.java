9bc021d0b40ee6eba499e56b2a1c6184d33356b7
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 14:07:29 +0100

    Add fault tolerance with Circuit Breaker pattern using Hystrix

--- src/main/java/com/joseluisestevez/msa/items/controllers/ItemController.java
+++ src/main/java/com/joseluisestevez/msa/items/controllers/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joseluisestevez.msa.items.models.Item;
+import com.joseluisestevez.msa.items.models.Product;
 import com.joseluisestevez.msa.items.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -23,8 +25,20 @@ public class ItemController {
 		return itemService.findAll();
 	}
 
+	@HystrixCommand(fallbackMethod = "fakeViewWithCircuitBreaker")
 	@GetMapping("/detail/{id}/quantity/{quantity}")
 	public Item detail(@PathVariable Long id, @PathVariable Integer quantity) {
 		return itemService.findById(id, quantity);
+	}
+
+	public Item fakeViewWithCircuitBreaker(Long id, Integer quantity) {
+		Product product = new Product();
+		product.setId(id);
+		product.setName("Product not avilable");
+		product.setPrice(0.0);
+		Item item = new Item();
+		item.setProduct(product);
+		item.setQuantity(quantity);
+		return item;
 	}
 }
