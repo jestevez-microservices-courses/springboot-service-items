02d2d4c51ea49221b8b494f51d9b163a6ab1c845
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 13:00:56 +0100

    Enable Eureka Client - Ribbon settings removed

--- src/main/java/com/joseluisestevez/msa/items/SpringbootServiceItemsApplication.java
+++ src/main/java/com/joseluisestevez/msa/items/SpringbootServiceItemsApplication.java
@@ -2,12 +2,12 @@ package com.joseluisestevez.msa.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-@RibbonClient(name = "service-products")
 public class SpringbootServiceItemsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServiceItemsApplication.class, args);
