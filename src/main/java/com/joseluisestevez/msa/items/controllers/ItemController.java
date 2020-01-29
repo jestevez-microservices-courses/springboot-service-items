d447864995173a9c6547e2b2e569add410a9cec2
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-29 17:37:17 +0100

    Verify the config-server is being used with an endpoint

--- src/main/java/com/joseluisestevez/msa/items/controllers/ItemController.java
+++ src/main/java/com/joseluisestevez/msa/items/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.joseluisestevez.msa.items.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +22,15 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ItemController.class);
 
 	@Autowired
 	@Qualifier("itemFeignService")
 	private ItemService itemService;
+
+	@Value("${msa.configuration.text}")
+	private String text;
 
 	@GetMapping("/list")
 	public List<Item> list() {
@@ -40,5 +52,17 @@ public class ItemController {
 		item.setProduct(product);
 		item.setQuantity(quantity);
 		return item;
+	}
+
+	@GetMapping("/get-config")
+	public ResponseEntity<Map<String, Object>> getConfig(
+			@Value("${server.port}") Long port) {
+
+		LOGGER.info("text=[{}], port=[{}]", text, port);
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("text", text);
+		map.put("port", port);
+		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
 	}
 }
